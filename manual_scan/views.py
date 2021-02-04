de3182947d23fb2b59f3000be0326934feec00b2
root <root@LAPTOP-N5DL6NV2.localdomain>
2021-02-04 23:19:16 +0800

    .

--- manual_scan/views.py
+++ manual_scan/views.py
@@ -36,14 +36,10 @@ def list_scan(request):
     :return:
     """
     username = request.user.username
-    project_id = request.POST.get('project_id')
     if request.method == 'POST':
-        print("***********************************************************************")
-        print(str(project_id))
+        project_id = request.POST.get('proj_id', )
         all_scans = manual_scans_db.objects.filter(username=username, project_id=project_id)
     else:
-        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
-        print(project_id)
         all_scans = manual_scans_db.objects.filter(username=username)
     all_projects = project_db.objects.filter(username=username)
 
